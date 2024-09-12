59ddf251bc82fe7499eadadfd3a56e88578b06d0
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-09-12 10:34:44 +0530

    Read me

---
+++ Pra_1.py
@@ -0,0 +1,18 @@
+#Write a programe to create one array from another array.
+
+x = [24 , 27 , 30 , 29 , 18 , 14]
+print("Orignal array")
+print(x)
+
+y = []
+y[:] = x
+print("New array")
+print(y)
+
+
+
+#output:-
+# Orignal array
+# [24, 27, 30, 29, 18, 14]
+# New array
+# [24, 27, 30, 29, 18, 14]
