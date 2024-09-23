05ea50ff2cc99559e588bf86f6f8a40508fbb1b0
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-09-23 19:57:53 +0530

    Read me

---
+++ Pra_8.py
@@ -0,0 +1,16 @@
+# 👉Write a programe to pass a list to a function and display it.
+
+def display_list(list):
+    for i in list:
+        print(i)
+
+list = [1,2,3,4,5,6]
+display_list(list)
+
+#output:-
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
