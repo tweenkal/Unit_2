2120283d77236b3c35c48fa3eebded54318b75d4
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-09-23 19:24:55 +0530

    Read me

---
+++ Pra_5.py
@@ -0,0 +1,12 @@
+# 👉Create a programe to search the position of an element in an
+#   array using index() method of array class.
+
+
+list = ['hello' , 'A' , 'B' , 'C' , 'D' , 'E']
+print(list)
+position = list.index('D')
+print(position)
+
+#output:-
+# ['hello', 'A', 'B', 'C', 'D', 'E']
+# 4
