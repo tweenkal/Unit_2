11755a95394057638cd1943f80d86342c22a4d1b
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-10-14 12:32:02 +0530

    [Read me]

--- Pra_11.py
+++ Pra_11.py
@@ -1,9 +1,9 @@
 #👉Write a lambda/Anonymous function to find bigger number in two
 #   given numbers.
+from turtledemo.sorting_animate import ssort
 
 val = lambda  x , y :max(x,y)
 print(val(120,179))
-
 
 
 #👉output:-
